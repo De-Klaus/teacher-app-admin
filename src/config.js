3cbb82f0328022a6981030475772275a88783074
petko_isands <petko@isands.ru>
2025-10-23 00:49:34 +0700

    add new backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // Centralized application configuration
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://teacherappthisdocker.onrender.com';
 export const AUTH_TOKEN_KEY = 'auth_token';
 export const AUTH_REFRESH_TOKEN_KEY = 'auth_refresh_token';
 
